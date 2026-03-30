95152df3dfff86016061e97ecd889a3cfd781645
nitinsharma450 <nitinsharma1059@gmail.com>
2026-03-30 22:20:58 +0530

    added connection.js file

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookies from 'cookie-parser';
 
+dotenv.config();
+
 const app=express();
 app.use(express.cors());
 app.use(express.json());
 
+const PORT=process.env.PORT;
 
-app.listen(5000, ()=>{
-    console.log("server is running on port 5000")
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
