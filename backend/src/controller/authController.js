34ed7de90f70ca993e89d5e43d85edc612e7f8fe
nitinsharma450 <nitinsharma1059@gmail.com>
2026-03-31 15:50:41 +0530

    added phoneNo service.js file

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,11 +1,23 @@
 import optGenerator from '../utils/optGenerator.js'
 import User from '../model/User.js'
 
-async function sendOpt(req,res){
+async function sendOtp(req,res){
   let {phoneNo,email,PhoneSuffix}=req.body;
+  let user;
+
+  if(!phoneNo || !PhoneSuffix){
+    return res.status(400).send({message:'phone number and phone suffix are required'})
+  }
+
+  user=await User.findOne({phoneNo:phoneNo})
+  if(!user){
+    user=new User({phoneNo:phoneNo,PhoneSuffix:PhoneSuffix})
+  }
+  user.save();
+
 let opt=await optGenerator()
 
-let user;
+
 if(email){
     let user=await User.findOne({email:email})
     if(!user){
@@ -15,7 +27,6 @@ if(email){
 user.emailOtpExpiry=Date.now()+1000*60*5;
 
 await user.save()
-
 
 return res.status(200).send({message:'opt send successfully to you email'})
 }
---
+++ backend/src/services/phoneNoService.js
@@ -0,0 +1,45 @@
+import twilio from 'twilio'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const serviceId=process.env.TWILIO_SERVICE_SID;
+const accountSid=process.env.TWILIO_ACCOUNT_SID;
+const authToken=process.env.TWILIO_AUTH_TOKEN;          
+
+const client=twilio(accountSid,authToken);
+
+export async function sendOptToPhone(phoneNo){
+ try {
+    if(!phoneNo){
+         throw new Error('phone number is required')
+    }
+
+    const response=await client.verify.services(serviceId).verifications.create({
+        to:phoneNo,
+        channel:'sms'
+    })
+    return response;
+ } catch (error) {
+    console.error('Failed to send OTP:', error.message);
+    throw error;
+ }
+}
+
+export async function verifyOpt(phoneNo,otp){
+console.log(otp);
+
+try {
+    const response=await client.verify.services(serviceId).verificationChecks.create({
+        to:phoneNo,
+        code :otp
+    })
+    
+    console.log(response)
+    return response
+} catch (error) {
+    console.lof(error.message)
+    throw new error('otp verfication failed!');
+}
+
+
+}
