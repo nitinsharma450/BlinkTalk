14f6ca6d794abe4a9c4c142da431ffedf53e2c6e
nitinsharma450 <nitinsharma1059@gmail.com>
2026-04-10 09:28:05 +0530

    code updated

--- Frontend/src/pages/loginSection/Login.tsx
+++ Frontend/src/pages/loginSection/Login.tsx
@@ -4,15 +4,14 @@ import countries from "../../utils/Contriles";
 import useLoginStore from "../../store/useLoginStore";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useNavigate, useNavigation } from "react-router";
+import { useNavigate } from "react-router";
 import useUserStore from "../../store/useUserStore";
 import { useForm } from "react-hook-form";
 import { avatars } from "../../utils/data";
 import { useThemeStore } from "../../store/useThemeStore";
 import { motion } from "framer-motion";
-import { FaChevronDown, FaUser, FaWhatsapp } from "react-icons/fa";
-import { p } from "framer-motion/client";
-import { CgSpinner } from "react-icons/cg";
+import { FaArrowLeft, FaChevronDown, FaUser, FaWhatsapp } from "react-icons/fa";
+
 import Spinner from "../../utils/Spinner";
 import { toast } from "react-toastify";
 
@@ -32,7 +31,8 @@ export default function Login() {
   const [avatar, setAvatar] = useState(avatars[0]);
   const [showDropDown, setShowDropDown] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [loading,setLoading]=useState(false)
+  const [loading, setLoading] = useState(false);
+  const [otpValue, setOtpValue] = useState("");
   const navigate = useNavigate();
   const { setUser } = useUserStore();
 
@@ -101,27 +101,48 @@ export default function Login() {
         email,
         phoneSuffix,
       });
-      
+
       console.log(response.data);
-      return response.data
+      return response.data;
     } catch (error) {
       console.log(error);
     }
   }
 
-  async function loginHandle(){
-    if(email){
-     let response=await sendOtp(null,email,null)
-     if(response.data.message==='success'){
-      toast.info("Opt send to you email")
+  async function loginHandle() {
+    try {
+      setLoading(true);
+      if (email) {
+        let response = await sendOtp(
+          phoneNumber,
+          null,
+          selectedCountry.dialCode,
+        );
+        if (response.data.message === "success") {
+          toast.info("Opt send to you phone number");
 
-      setUserPhoneData({email})
-      setStep(2)
-     }
+          setUserPhoneData({
+            phoneNumber,
+            phoneSuffix: selectedCountry.dialCode,
+          });
+          setStep(2);
+        }
+      } else {
+        let response = await sendOtp(null, email, null);
+        if (response.data.message === "success") {
+          toast.info("Opt send to you email");
+
+          setUserPhoneData({ email });
+          setStep(2);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setError("fail to send otp");
+    } finally {
+      setLoading(false);
     }
-   
   }
-
   async function verifyOtp(
     phoneNo: any,
     email: any,
@@ -135,6 +156,7 @@ export default function Login() {
       phoneSuffix,
     });
     console.log(response?.data.message);
+    return response?.data;
   }
 
   async function checkAuth() {
@@ -158,6 +180,76 @@ export default function Login() {
       console.log(error);
     }
   }
+
+  const handleOtpChange = (index: any, value: any) => {
+    // Copy OTP array
+    const newOtp = [...otp];
+
+    // Update value
+    newOtp[index] = value;
+
+    // Update state
+    setOtp(newOtp);
+
+    // Convert array to string (for form/backend)
+    setOtpValue(newOtp.join(""));
+
+    if (value && index < 5) {
+      document.getElementById(`otp-${index + 1}`)?.focus();
+    }
+  };
+
+  const handleBack = () => {
+    setStep(1);
+    setUserPhoneData(null);
+    setOtp(["", "", "", "", "", ""]);
+    setError("");
+  };
+
+  const onOtpSubmit = async () => {
+    try {
+      setLoading(true);
+
+      if (!userPhoneData) {
+        throw new Error("Phone or email data is missing");
+      }
+
+      const otpString = otp.join("");
+
+      let response;
+
+      if (userPhoneData.email) {
+        response = await verifyOtp(null, userPhoneData.email, otpString, null);
+      } else {
+        response = await verifyOtp(
+          userPhoneData.phoneNumber,
+          null,
+          otpString,
+          userPhoneData.phoneSuffux.dialCode,
+        );
+      }
+
+      if (response.message === "success") {
+        toast.success("OTP verify successfully");
+
+        const user = response?.user;
+
+        if (user?.userName && user?.profilePicture) {
+          setUser(user);
+          toast.success("Welcome back to Whatsapp");
+
+          navigate("/");
+          resetLoginState();
+        } else {
+          setStep(3);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const ProgressBar = () => (
     <div
@@ -209,7 +301,7 @@ export default function Login() {
           <ProgressBar />
 
           {step === 1 && (
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={loginHandle}>
               <p
                 className={`text-center ${theme === "dark" ? "text-gray-300" : "text-gray-600"} mb-4`}
               >
@@ -308,21 +400,65 @@ export default function Login() {
                   onChange={(e) => setEmail(e.target.value)}
                   placeholder="Email (optional)"
                   className={`w-full px-4 py-2 border ${
-                    theme === "dark"
-                      ? " text-white"
-                      : "bg-white"
-                  }${
-                    loginErrors.phoneNumber ? "border-red-500" : ""
-                  }`}
+                    theme === "dark" ? " text-white" : "bg-white"
+                  }${loginErrors.phoneNumber ? "border-red-500" : ""}`}
                 />
               </div>
-               {loginErrors.email && (
+              {loginErrors.email && (
+                <p className="text-red-500 text-sm">
+                  {loginErrors.email.message}
+                </p>
+              )}
+
+              <button className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition">
+                {loading ? <Spinner /> : "Send Otp"}
+              </button>
+            </form>
+          )}
+
+          {step == 2 && (
+            <form action="">
+              <p>
+                The 6-digit OTP sent to your{" "}
+                {userPhoneData?.phoneSuffix ? "phone" : "Email"} (
+                {userPhoneData?.phoneNumber})
+              </p>
+              <div className="flex justify-between">
+                {otp.map((digit, index) => (
+                  <input
+                    key={index}
+                    id={`otp-${index}`}
+                    type="text"
+                    maxLength={1}
+                    value={digit}
+                    onChange={(e) => handleOtpChange(index, e.target.value)}
+                    className={`w-12 h-12 text-center border ${
+                      theme === "dark"
+                        ? "bg-gray-700 border-gray-600 text-white"
+                        : "bg-white border-gray-300"
+                    } rounded-md focus:outline-none focus:ring-2 focus:ring-green-500`}
+                  />
+                ))}
+
+              
+              </div>
+                {otpErrors.otp && (
                   <p className="text-red-500 text-sm">
-                    {loginErrors.email.message}
+                    {otpErrors.otp.message}
                   </p>
                 )}
 
-                <button className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition">{!loading ?<Spinner />:'Send Otp'}</button>
+                <button className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition">
+                  {loading ? <Spinner /> : "verify Otp"}
+                </button>
+                
+                <button
+                  onClick={handleBack}
+                  className={`w-full mt-2 ${theme === "dark" ? "bg-gray-500 text-gray-600" : "bg-gray-500 text-black"} py-2  rounded-md hover:bg-gray-300 transition flex items-center justify-center`}
+                >
+                  
+                  <FaArrowLeft className="mr-2" /> Back
+                </button>
             </form>
           )}
         </motion.div>
